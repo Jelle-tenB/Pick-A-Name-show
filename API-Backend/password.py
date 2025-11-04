096fe1e73162c1b5c4404a1dcfd9d763ab80b7b8
Jelle <jellepelle214@gmail.com>
2025-11-04 14:57:25 +0100

    Update password.py

--- API-Backend/password.py
+++ API-Backend/password.py
@@ -1,7 +1,7 @@
-from passlib.context import CryptContext
+"""Normally this would encrypt the password but can't show it for obvious reasons"""
 
-pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 def hash_pwd(password: str) -> str:
     """Hash the password"""
 
-    return pwd_context.hash(password)
+    return password
+
